f154c186607611f26eac2f3048bd6dc0d91b65dd
pavelerokhin <pavel.erokhin@rstor.io>
2022-06-09 21:28:47 +0200

    insert API, insert-random API

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	e.GET("/ws", h.NewsFeedWebSocketHandler)
 	e.GET("/csv", h.DownloadCSV)
 	e.POST("/insert", h.Insert)
-	e.POST("/insert-random", h.InsertRandom)
+	e.POST("/insert-random", h.InsertRandom) // curl -X POST http://localhost:1111/insert-random
 
 	e.Logger.Fatal(e.Start(":1111"))
 }
